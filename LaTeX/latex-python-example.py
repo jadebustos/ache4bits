e3f61cbfe3fe5c924460410cd31595092902ed20
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2022-05-22 01:13:08 +0200

    latex and python

--- LaTeX/latex-python-example.py
+++
@@ -1,9 +0,0 @@
-## a ----> c
-# b ----> x
-#
-def proporcionality(a, b, c, unit):
-  sol = (c*b/a)
-  print(r'Peter is paid %d %s \ for working %d hours. He will be paid %.2f %s \ for working %d hours.' % (b, unit, a, sol, unit, c))
-
-proporcionality(10, 120, 40, '\\texteuro')
-
